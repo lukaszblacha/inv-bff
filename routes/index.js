48379d4ac942e0b56639f5070768f52f386cbd34
Łukasz Blacha <lukasz.blacha@schibsted.pl>
2016-03-18 10:34:30 +0100

    config

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('good-guy-http')({maxRetries: 3});
 var jp = require('jsonpath');
 
-var booksServiceUrl = process.env.URL_BOOKS_SERVICE;
+var booksServiceUrl = process.env.URL_BOOKS_SERVICE || 'https://book-catalog-proxy-3.herokuapp.com';
 var inventoryServiceUrl = process.env.URL_INVENTORY_SERVICE || 'http://localhost:3000';
 
 /* GET home page. */
@@ -11,7 +11,6 @@ router.get('/:isbn', function(req, res, next) {
   request(booksServiceUrl + '/book?isbn=' + req.params.isbn).then(function(response) {
     var b = JSON.parse(response.body);
 
-    console.log(JSON.stringify(b, null, 4));
     res.render('index', {
       title: jp.value(b, '$..title'),
       thumbnail: jp.value(b, '$..thumbnail'),
