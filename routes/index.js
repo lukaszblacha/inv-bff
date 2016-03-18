7a31e75fa176b820910c2717ac57ee0125729f2f
Łukasz Blacha <lukasz.blacha@schibsted.pl>
2016-03-18 10:02:07 +0100

    config

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,20 @@ var router = express.Router();
 var request = require('good-guy-http')({maxRetries: 3});
 var jp = require('jsonpath');
 
-var url = 'https://book-catalog-proxy-5.herokuapp.com/book?isbn=';
+var booksServiceUrl = process.env.URL_BOOKS_SERVICE;
+var inventoryServiceUrl = process.env.URL_INVENTORY_SERVICE || 'http://localhost:3000';
 
 /* GET home page. */
 router.get('/:isbn', function(req, res, next) {
-  request(url + req.params.isbn).then(function(response) {
+  request(booksServiceUrl + '/book?isbn=' + req.params.isbn).then(function(response) {
     var b = JSON.parse(response.body);
 
     console.log(JSON.stringify(b, null, 4));
     res.render('index', {
       title: jp.value(b, '$..title'),
       thumbnail: jp.value(b, '$..thumbnail'),
-      isbn: req.params.isbn
+      isbn: req.params.isbn,
+      availabilityCheckUrl: inventoryServiceUrl + '/stock/' + req.params.isbn
     });
   });
 });
